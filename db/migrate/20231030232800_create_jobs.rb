b7872ff57c2b5fd3cde8084c5d95e3f86ca72126
Nick Dower <nicholasdower@gmail.com>
2023-10-30 23:30:27 +0100

    Add a DB migration.

---
+++ db/migrate/20231030232800_create_jobs.rb
@@ -0,0 +1,23 @@
+class CreateJobs < ActiveRecord::Migration[7.1]
+  def self.up
+    create_table :jobs do |table|
+      table.string   :job_class,            null: false
+      table.string   :job_id,               null: false
+      table.string   :queue_name
+      table.integer  :priority
+      table.text     :arguments,            null: false
+      table.integer  :executions,           null: false
+      table.text     :exception_executions
+      table.string   :locale,               null: false
+      table.string   :timezone,             null: false
+      table.datetime :enqueued_at,          null: false
+      table.datetime :scheduled_at
+
+      table.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :jobs
+  end
+end
