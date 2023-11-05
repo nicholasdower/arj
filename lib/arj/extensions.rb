46124335dc42c928c6b62f151721e181c087b7fa
Nick Dower <nicholasdower@soundcloud.com>
2023-11-05 22:48:34 +0100

    Add missing documentation.

--- lib/arj/extensions.rb
+++ lib/arj/extensions.rb
@@ -3,7 +3,9 @@
 require_relative 'extensions/last_error'
 require_relative 'extensions/shard'
 
-# Arj extensions:
-# - {Arj::Extensions::Shard}     - Adds a +shard+ attribute to a job class.
-# - {Arj::Extensions::LastError} - Adds a +last_error+ attribute to a job class.
-module Extensions; end
+module Arj
+  # Arj extensions:
+  # - {Arj::Extensions::Shard}     - Adds a +shard+ attribute to a job class.
+  # - {Arj::Extensions::LastError} - Adds a +last_error+ attribute to a job class.
+  module Extensions; end
+end
--- lib/arj/version.rb
+++ lib/arj/version.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 module Arj
+  # The Arj version.
   VERSION = '0.0.0'
 end
